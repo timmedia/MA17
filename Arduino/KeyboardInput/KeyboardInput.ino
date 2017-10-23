24479905851d7da2d5aacb5d7bbac6359210af44
Tim <contact@tim-media.com>
2017-10-23 15:33:02 +0200

    Update KeyboardInput.ino

--- Arduino/KeyboardInput/KeyboardInput.ino
+++ Arduino/KeyboardInput/KeyboardInput.ino
@@ -1,40 +1,43 @@
 // Tastatur Bibliothek
 #include <Keyboard.h>
 
-// Variablen, Werte werden vor dem Kompilieren ersetzt
+// Variablen, Werte werden vor dem Kompilieren im Code mit den Werten ersetzt
 #define buttonA 6   // A an Pin 6
 #define buttonD 7   // D an Pin 7
 #define buttonSpace 8   // Leertaste an Pin 8 
 #define buttonShift 9   // Leertaste an Pin 9
 
-int characters[4] = {97, 100, 32, 129};
-boolean isDown[4] = {false, false, false, false};
-boolean wasDown[4];
+int characters[4] = {97, 100, 32, 129};           // Charaktere nach ASCII (a, d, Leertaste, shift)
+boolean isDown[4] = {false, false, false, false}; // Array mit Status aller Tasten
+boolean wasDown[4];                               // Array mit vorherigen Status
 
+// Initialisierungs-Funktion, wird bei Arduino-start 1x durchlaufen
 void setup() {
-  Keyboard.begin();
-  pinMode(buttonA, INPUT_PULLUP);
-  pinMode(buttonD, INPUT_PULLUP);
+  Keyboard.begin();                   // Tastatur Emulierung wird gestartet
+  pinMode(buttonA, INPUT_PULLUP);     // Pins als Tasten-Input, interner Widerstand wird verwendet
+  pinMode(buttonD, INPUT_PULLUP);     
   pinMode(buttonSpace, INPUT_PULLUP);
   pinMode(buttonShift, INPUT_PULLUP);
 }
 
+// Endlosschleife, wird nach setup() endlos ausgeführt
 void loop() {
-  memcpy(wasDown, isDown, 4);
-  isDown[0] = !digitalRead(buttonA);
+  memcpy(wasDown, isDown, 4);        // Array von vorherigen Status wird kopiert & gespeichert
+  isDown[0] = !digitalRead(buttonA); // Status von Pins wird überprüft und gespeichert
   isDown[1] = !digitalRead(buttonD);
   isDown[2] = !digitalRead(buttonSpace);
   isDown[3] = !digitalRead(buttonShift);
-  for (int k = 0; k < 4; k++) {
-    processKey(k);
-  }
-  delay(1);
+  for (int k = 0; k < 4; k++) {processKey(k);} // Jede Taste wird verarbeitet
+  delay(1); // Verzögerung von 1ms für Stabilität
 }
 
+// Funktion vom Taste zu verarbeiten
 void processKey(int i) {
-  if (isDown[i] && !wasDown[i]) {
-    Keyboard.press(characters[i]);
-  } else if (!isDown[i] && wasDown[i]) {
+  if (isDown[i] && !wasDown[i]) { // Knopf ist unten & war nicht unten
+    // Tastatur Input soll bloss betätigt werden falls Taste vorher noch nicht gedrückt wurde
+    Keyboard.press(characters[i]); 
+  } else if (!isDown[i] && wasDown[i]) { // Knopf war unten & ist nicht unten
+    // Tastatur Input soll lossgelassen werden
     Keyboard.release(characters[i]);
   }
 }
