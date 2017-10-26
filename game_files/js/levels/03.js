b52967fb6f56329ca4e59269e725919ed9500a81
Tim <contact@tim-media.com>
2017-10-26 09:15:55 +0200

    Update 03.js

--- game_files/js/levels/03.js
+++ game_files/js/levels/03.js
@@ -11,24 +11,28 @@ class Level03 extends GameState {
       'Level03 Background'
     )
     this.player = new Player(this, 225, 1850, 'Player 01', 300, -600, true)
+    // Spieler soll sterben beim Verlassen des unteren Weltrandes, jedoch zum nächsten Level gehen beim Verlassen oben
     this.player.checkWorldBounds = true
     this.player.events.onOutOfBounds.add(() => {
       if (this.player.y > 0) {
-        this.damagePlayer()
+        this.killPlayer()
       } else {
-        this.player.body.allowGravity = false // Spieler fällt nicht wieder nach unten
+        this.player.body.allowGravity = false // Spieler bleibt oben (verschwunden)
         this.goToNextLevel()
       }
     })
+    // Wasseroberfläche als dynamisches Objekt mit Animation
     this.water = new DynamicGameObject(this, 120, 2100, 'Level03 Waves')
     this.water.animations.add('flowing', [0, 1, 2, 3, 4], 5, true)
     this.water.animations.play('flowing')
+    // Wasser unterhalb Oberfäche separat, nicht animiert, als child damit Bewgung gleich
     this.water.addChild(new BasicGameObject(this, 0, 431, 'Level03 Water'))
-    this.water.body.allowGravity = false
-    this.water.body.velocity.y = - 40
-    this.water.body.setSize(560, 100, 0, 50)
+    this.water.body.allowGravity = false // keine Schwerkraft
+    this.water.body.velocity.y = - 40    // konstante Bewegung nach oben
+    this.water.body.setSize(560, 100, 0, 50) // Hitbox verkleinert, 
   }
   loop() {
+    // Kollision zwischen Wasser und Spieler überprüfen
     this.physics.arcade.overlap(this.player, this.water, this.player.damage, null, this)
   }
 }
