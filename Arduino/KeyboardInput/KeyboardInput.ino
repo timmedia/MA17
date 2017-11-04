bd6c33d8956532e92f1b02e8b202ee969d5e4d6a
Tim <contact@tim-media.com>
2017-11-04 16:04:35 +0100

    Update KeyboardInput.ino

--- Arduino/KeyboardInput/KeyboardInput.ino
+++ Arduino/KeyboardInput/KeyboardInput.ino
@@ -6,8 +6,8 @@
 #define buttonBackspace 16 // Backspace an Pin 5
 #define buttonA 14         // A an Pin 6
 #define buttonD 15         // D an Pin 7
-#define buttonSpace A0     // Leertaste an Pin 8 
-#define buttonShift A1     // Shift-Taste an Pin 9
+#define buttonSpace 5      // Leertaste an Pin 8 
+#define buttonShift 9      // Shift-Taste an Pin 9
 
 int characters[6] = {177, 178, 97, 100, 32, 129};               // Charaktere nach ASCII (a, d, Leertaste, shift)
 boolean isDown[6] = {false, false, false, false, false, false}; // Array mit Status aller Tasten
@@ -18,8 +18,8 @@ void setup() {
   Keyboard.begin();                 // Tastatur Emulierung wird gestartet
   pinMode(buttonEsc, INPUT_PULLUP); // Pins als Tasten-Input, interner Widerstand wird verwendet
   pinMode(buttonBackspace, INPUT_PULLUP);
-  pinMode(buttonA, INPUT_PULLUP);     
-  pinMode(buttonD, INPUT_PULLUP);     
+  pinMode(buttonA, INPUT_PULLUP);
+  pinMode(buttonD, INPUT_PULLUP);
   pinMode(buttonSpace, INPUT_PULLUP);
   pinMode(buttonShift, INPUT_PULLUP);
 }
@@ -31,11 +31,13 @@ void loop() {
   // Status von Pins wird überprüft und gespeichert
   isDown[0] = !digitalRead(buttonEsc);
   isDown[1] = !digitalRead(buttonBackspace);
-  isDown[2] = !digitalRead(buttonA); 
+  isDown[2] = !digitalRead(buttonA);
   isDown[3] = !digitalRead(buttonD);
   isDown[4] = !digitalRead(buttonSpace);
   isDown[5] = !digitalRead(buttonShift);
-  for (int k = 0; k < 6; k++) { processKey(k); } // Jede Taste wird verarbeitet
+  for (int k = 0; k < 6; k++) {
+    processKey(k);  // Jede Taste wird verarbeitet
+  }
   delay(1); // Verzögerung von 1ms für Stabilität
 }
 
@@ -43,11 +45,10 @@ void loop() {
 void processKey(int i) {
   if (isDown[i] && !wasDown[i]) { // Knopf ist unten & war nicht unten
     // Tastatur Input soll bloss betätigt werden falls Taste vorher noch nicht gedrückt wurde
-    Keyboard.press(characters[i]); 
+    Keyboard.press(characters[i]);
   } else if (!isDown[i] && wasDown[i]) { // Knopf war unten & ist nicht unten
     // Tastatur Input soll lossgelassen werden
     Keyboard.release(characters[i]);
   }
 }
-
 
