71f3553f98ea0fb4a4b2dba92226362fef8ff9ed
Tim <contact@tim-media.com>
2017-11-04 16:47:59 +0100

    Update KeyboardInput.ino

--- Arduino/KeyboardInput/KeyboardInput.ino
+++ Arduino/KeyboardInput/KeyboardInput.ino
@@ -2,12 +2,12 @@
 #include <Keyboard.h>
 
 // Variablen, Werte werden vor dem Kompilieren im Code mit den Werten ersetzt
-#define buttonEsc 10       // Escape an Pin 4
-#define buttonBackspace 16 // Backspace an Pin 5
-#define buttonA 14         // A an Pin 6
-#define buttonD 15         // D an Pin 7
-#define buttonSpace 5      // Leertaste an Pin 8 
-#define buttonShift 9      // Shift-Taste an Pin 9
+#define buttonEsc 4       // Escape an Pin 4
+#define buttonBackspace 5 // Backspace an Pin 5
+#define buttonA 6         // A an Pin 6
+#define buttonD 7         // D an Pin 7
+#define buttonSpace 10    // Leertaste an Pin 8 
+#define buttonShift 14     // Shift-Taste an Pin 9
 
 int characters[6] = {177, 178, 97, 100, 32, 129};               // Charaktere nach ASCII (a, d, Leertaste, shift)
 boolean isDown[6] = {false, false, false, false, false, false}; // Array mit Status aller Tasten
@@ -15,29 +15,34 @@ boolean wasDown[6];                                             // Array mit vor
 
 // Initialisierungs-Funktion, wird bei Arduino-start 1x durchlaufen
 void setup() {
+  Serial.begin(9600);
   Keyboard.begin();                 // Tastatur Emulierung wird gestartet
   pinMode(buttonEsc, INPUT_PULLUP); // Pins als Tasten-Input, interner Widerstand wird verwendet
   pinMode(buttonBackspace, INPUT_PULLUP);
-  pinMode(buttonA, INPUT_PULLUP);
-  pinMode(buttonD, INPUT_PULLUP);
+  pinMode(buttonA, INPUT_PULLUP);     
+  pinMode(buttonD, INPUT_PULLUP);     
   pinMode(buttonSpace, INPUT_PULLUP);
   pinMode(buttonShift, INPUT_PULLUP);
 }
 
 // Endlosschleife, wird nach setup() endlos ausgeführt
 void loop() {
-  memcpy(wasDown, isDown, 4);        // Array von vorherigen Status wird kopiert & gespeichert
+  Serial.print(isDown[0]);
+  Serial.print(isDown[1]);
+  Serial.print(isDown[2]);
+  Serial.print(isDown[3]);
+  Serial.print(isDown[4]);
+  Serial.println(isDown[5]);
+  memcpy(wasDown, isDown, 6);        // Array von vorherigen Status wird kopiert & gespeichert
 
   // Status von Pins wird überprüft und gespeichert
   isDown[0] = !digitalRead(buttonEsc);
   isDown[1] = !digitalRead(buttonBackspace);
-  isDown[2] = !digitalRead(buttonA);
+  isDown[2] = !digitalRead(buttonA); 
   isDown[3] = !digitalRead(buttonD);
   isDown[4] = !digitalRead(buttonSpace);
   isDown[5] = !digitalRead(buttonShift);
-  for (int k = 0; k < 6; k++) {
-    processKey(k);  // Jede Taste wird verarbeitet
-  }
+  for (int k = 0; k < 6; k++) { processKey(k); } // Jede Taste wird verarbeitet
   delay(1); // Verzögerung von 1ms für Stabilität
 }
 
@@ -45,10 +50,11 @@ void loop() {
 void processKey(int i) {
   if (isDown[i] && !wasDown[i]) { // Knopf ist unten & war nicht unten
     // Tastatur Input soll bloss betätigt werden falls Taste vorher noch nicht gedrückt wurde
-    Keyboard.press(characters[i]);
+    Keyboard.press(characters[i]); 
   } else if (!isDown[i] && wasDown[i]) { // Knopf war unten & ist nicht unten
     // Tastatur Input soll lossgelassen werden
     Keyboard.release(characters[i]);
   }
 }
+
 
